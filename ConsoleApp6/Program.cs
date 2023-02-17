49cae5d74688d7e3ee4e6203855afad3bd6ff414
Admin <Admin@TEB127010>
2023-02-17 10:07:27 +0100

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static int AREA(int höjd, int bas)
+    {
+        return höjd*bas;
+    }
+    static void Main()
+    {
+        Console.Write("Skriv Höjden för rektanglen ");
+        int Höjd = int.Parse(Console.ReadLine());
+        Console.Write("Skriv Basen för rektanglen ");
+        int Bas = int.Parse(Console.ReadLine());
+
+        int area = AREA(Höjd, Bas);
+        Console.WriteLine("Arean på rektanglen är " + area);
+    }
+}
